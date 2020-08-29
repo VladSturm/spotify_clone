93b4b1a9df2c6448f069fb62fe3bda241c3a123f
Vladsturm <sturmdob1994@gmail.com>
2020-08-29 16:21:14 +0200

    body

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Body.css';
+import Header from './Header';
 
-function Body() {
+
+function Body({spotify}) {
     return (
         <div className='body'>
-            <h1> I am the body</h1>
+            <Header spotify={spotify}/>
+
+            <div className="body_info">
+                <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
+                <div className="body_infoText">
+                    <strong>PLAYLIST</strong>
+                    <h2>Dissover Weekly</h2>
+                    <p>Description...</p>
+                </div>
+            </div>
         </div>
     )
 }
